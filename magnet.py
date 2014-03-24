4a31c59a7baa2b7eb723d366f39cf43e35bef759
starky00 <starky_00@hotmail.com>
2014-03-24 11:19:26 +0100

    Update magnet.py

--- magnet.py
+++ magnet.py
@@ -4,20 +4,33 @@
 #  Modulos
 
 import libtorrent as lt
-import time, os, threading
-
+import time, threading
+import os
+import re
 
 PATH = "/tmp/data_m/"
+
 
 def temporal():
 	os.system("mkdir -p "+PATH)
 	
 def abrevideo():
+	
+
 	print "Abriendo video..."	
-	lectura_temp = os.popen("ls "+PATH).read() #utilizo popen porque no necesito conocer si ha finalizado correctamente y asi me ahorra el 0 o el 1
+#	lectura_temp = os.popen("ls "+PATH).read() #utilizo popen porque no necesito conocer si ha finalizado correctamente y asi me ahorra el 0 o el 1
+	lectura_temp = os.listdir(PATH)
+	lectura_path = ''.join(lectura_temp)
 	print lectura_temp 
-	os.system("mplayer /tmp/data_m/*.avi")
-	#os.system("rm -rf "+PATH)
+	if re.search('.*\.mp4', ''.join(lectura_temp)) or re.search('.*\.avi', ''.join(lectura_temp)):
+		os.system ('mplayer -fs -framedrop "'+PATH+''.join(lectura_temp)+'"')
+		
+	else:
+		print "directorio superior"
+		r2 = os.popen('ls '+'"'+PATH+''.join(lectura_temp)+'"').read()
+		if re.search('.*\.mp4', r2) or re.search('.*\.avi', r2):
+			os.system('mplayer -fs -framedrop  "'+PATH+lectura_path+"/"+re.search('.*\.mp4',r2).group()+'"')
+			
 
 	
 
@@ -54,4 +67,3 @@ def main():
 
 if __name__ == '__main__':
 	main()
-
